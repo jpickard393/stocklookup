d5d36bba2a344c94cee8d46c4516289e4b079cfc
jpickard393 <jpickard@and.digital>
2021-03-28 12:51:03 +0100

    style changes to news items

--- src/components/MarketNews/MarketNewsItem/index.jsx
+++ src/components/MarketNews/MarketNewsItem/index.jsx
@@ -5,10 +5,10 @@ const MarketNewsItem = ({headline, image, summary, url }) => {
     return (
         <div className="market-news-item">
                 <div className="market-news-item-top">
-                    <div className="market-news-image-container"><a href={url}><img className="market-news-image" src={image} alt="news Item"></img></a></div>
-                    <div className="market-news-headline-container"><a href={url} className="market-news-headline">{headline}</a></div>
+                    <div className="market-news-image-container"><a href={url} target="_blank" rel="noreferrer"><img className="market-news-image" src={image} alt="news Item"></img></a></div>
+                    <div className="market-news-headline-container"><a href={url} target="_blank" rel="noreferrer" className="market-news-headline">{headline}</a></div>
                 </div>
-                <div className="market-news-summary"><p className="market-news-summary-text">{summary}</p></div>
+                <div className="market-news-summary"><div className="market-news-summary-text">{summary}</div></div>
             </div>
     );
 };
